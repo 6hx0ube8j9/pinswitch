5ba4ed8bfee73d265df9ce9e2cf19850b31b0bf6
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-05-10 16:29:24 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/getlantern/systray"
+	"github.com/energye/systray"
 	"github.com/pkg/browser"
 )
 
@@ -10,30 +10,33 @@ func main() {
 }
 
 func onReady() {
-	// 设置一个空的图标（或者放你的图标字节）
-	systray.SetIcon([]byte{}) 
-	systray.SetTitle("Bing工具")
-	systray.SetTooltip("左键或菜单均可访问Bing")
+	// 创建一个简单的 16x16 红色像素图标字节流，防止 Windows 报错
+	// 这样你就不用手动上传 icon.ico 了
+	dummyIcon := []byte{
+		0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x10, 0x10, 0x00, 0x00, 0x01, 0x00, 0x20, 0x00, 0x68, 0x04,
+		0x00, 0x00, 0x16, 0x00, 0x00, 0x00, 0x28, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x20, 0x00,
+		0x00, 0x00, 0x01, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00,
+	}
 
-	// 1. 添加菜单项
+	systray.SetIcon(dummyIcon)
+	systray.SetTitle("Bing工具")
+	systray.SetTooltip("左键直接打开，右键弹出菜单")
+
 	mOpen := systray.AddMenuItem("打开浏览器", "访问 Bing")
 	mQuit := systray.AddMenuItem("退出", "退出程序")
 
-	// 2. 核心逻辑
 	go func() {
 		for {
 			select {
-			case <-mOpen.ClickedCh:
+			case <-systray.OnClickCh(): // 监听左键点击
+				openBing()
+			case <-mOpen.ClickedCh: // 监听菜单点击
 				openBing()
 			case <-mQuit.ClickedCh:
 				systray.Quit()
-			// 注意：systray 在某些版本中通过 ClickedCh 统一处理
-			// 如果库版本支持，左键点击图标也会触发默认菜单项
 			}
 		}
 	}()
-
-	// 技巧：有些 Windows 驱动下，第一个菜单项就是左键点击的默认行为
 }
 
 func openBing() {
