54b1bdfb3819af8b1222581e2ee0731eb494ddb8
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-05-10 16:49:29 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	_ "embed" // 必须引入 embed 包
+	_ "embed"
 	"github.com/energye/systray"
 	"github.com/pkg/browser"
 )
 
-// 使用 go:embed 将你的图标文件编译进二进制中
-// 路径必须相对于 main.go 所在的目录
 //go:embed icons/app.ico
 var iconData []byte
 
@@ -16,34 +14,27 @@ func main() {
 }
 
 func onReady() {
-	// 1. 设置你上传的图标
 	systray.SetIcon(iconData)
 	systray.SetTitle("Bing 工具")
-	systray.SetTooltip("左键直接进入 Bing，右键弹出菜单")
+	systray.SetTooltip("左键点击进入 Bing")
 
-	// 2. 左键单击图标事件
+	// 左键点击直接打开
 	systray.SetOnClick(func(menu systray.IMenu) {
-		openBing()
+		browser.OpenURL("https://www.bing.com/")
 	})
 
-	// 3. 右键菜单项
+	// 右键菜单
 	mOpen := systray.AddMenuItem("打开浏览器", "访问 Bing")
 	mOpen.Click(func() {
-		openBing()
+		browser.OpenURL("https://www.bing.com/")
 	})
 
 	systray.AddSeparator()
 
-	mQuit := systray.AddMenuItem("退出", "退出程序")
+	mQuit := systray.AddMenuItem("退出", "关闭程序")
 	mQuit.Click(func() {
 		systray.Quit()
 	})
 }
 
-func openBing() {
-	_ = browser.OpenURL("https://www.bing.com/")
-}
-
-func onExit() {
-	// 退出清理逻辑
-}
+func onExit() {}
