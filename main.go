9f11a64e3c06047dffe8367cc54173dea927da8d
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-05-10 17:26:10 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,24 +6,27 @@ import (
 	"github.com/pkg/browser"
 )
 
+// 显式指定图标路径，编译时会将其嵌入到 exe 内部
 //go:embed icons/app.ico
 var iconData []byte
 
 func main() {
+	// 启动托盘运行循环
 	systray.Run(onReady, onExit)
 }
 
 func onReady() {
+	// 设置托盘图标
 	systray.SetIcon(iconData)
 	systray.SetTitle("Bing 工具")
-	systray.SetTooltip("左键点击进入 Bing")
+	systray.SetTooltip("左键点击进入 Bing，右键弹出菜单")
 
-	// 左键点击直接打开
+	// 1. 处理左键点击图标 (Windows 专用事件)
 	systray.SetOnClick(func(menu systray.IMenu) {
 		browser.OpenURL("https://www.bing.com/")
 	})
 
-	// 右键菜单
+	// 2. 处理右键菜单
 	mOpen := systray.AddMenuItem("打开浏览器", "访问 Bing")
 	mOpen.Click(func() {
 		browser.OpenURL("https://www.bing.com/")
@@ -31,10 +34,12 @@ func onReady() {
 
 	systray.AddSeparator()
 
-	mQuit := systray.AddMenuItem("退出", "关闭程序")
+	mQuit := systray.AddMenuItem("退出程序", "关闭工具")
 	mQuit.Click(func() {
 		systray.Quit()
 	})
 }
 
-func onExit() {}
+func onExit() {
+	// 程序退出时的清理逻辑（可选）
+}
