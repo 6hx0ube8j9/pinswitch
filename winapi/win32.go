496b7a09896fe9ed694229e01fab5ae7cbf697e3
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-06-04 09:52:03 +0800

    Update win32.go

--- winapi/win32.go
+++ winapi/win32.go
@@ -19,9 +19,6 @@ var (
 	procPostQuitMessage          = user32.NewProc("PostQuitMessage")
 	procFindWindowW              = user32.NewProc("FindWindowW")
 	procSendMessageW             = user32.NewProc("SendMessageW")
-	// 【新增】引入异步投递消息和获取窗口进程 PID 的 API
-	procPostMessageW             = user32.NewProc("PostMessageW")
-	procGetWindowThreadProcessId = user32.NewProc("GetWindowThreadProcessId")
 	
 	advapi32                     = syscall.NewLazyDLL("advapi32.dll")
 	procRegOpenKeyEx             = advapi32.NewProc("RegOpenKeyExW")
@@ -48,9 +45,6 @@ const (
 	REG_NOTIFY_CHANGE_LAST_SET = 0x00000004
 	WAIT_OBJECT_0              = 0x00000000
 	HWND_MESSAGE               = ^uintptr(2)
-	WM_CLOSE                   = 0x0010
-	WM_HOTKEY                  = 0x0312
-	WM_DESTROY                 = 0x0002
 )
 
 type TagMSG struct {
@@ -75,7 +69,6 @@ type TagWNDCLASS struct {
 	LpszClassName *uint16
 }
 
-// 【优化】直接通过 Call 返回的最后一个参数安全获取系统全局错误码
 func CreateMutex(name string) (uintptr, error) {
 	namePtr, _ := syscall.UTF16PtrFromString(name)
 	ret, _, err := procCreateMutex.Call(0, 1, uintptr(unsafe.Pointer(namePtr)))
@@ -93,21 +86,8 @@ func SendMessage(hwnd uintptr, msg uint32, wparam, lparam uintptr) uintptr {
 	return ret
 }
 
-// 【新增】异步非阻塞投递消息
-func PostMessage(hwnd uintptr, msg uint32, wparam, lparam uintptr) bool {
-	ret, _, _ := procPostMessageW.Call(hwnd, uintptr(msg), wparam, lparam)
-	return ret != 0
-}
-
-// 【新增】获取窗口所属的进程 ID
-func GetWindowThreadProcessId(hwnd uintptr, pid *uint32) uint32 {
-	ret, _, _ := procGetWindowThreadProcessId.Call(hwnd, uintptr(unsafe.Pointer(pid)))
-	return uint32(ret)
-}
-
 func RegisterClass(className string, wndProc func(hwnd uintptr, msg uint32, wparam uintptr, lparam uintptr) uintptr) {
 	namePtr, _ := syscall.UTF16PtrFromString(className)
-	// 用 0 调用 CloseHandle 是不正确的习惯，此处直接传入 0 获取 hInstance
 	wc := TagWNDCLASS{
 		LpfnWndProc:   syscall.NewCallback(wndProc),
 		HInstance:     0,
