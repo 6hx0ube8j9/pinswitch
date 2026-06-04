862af8084bf508c5792d3a29d7e970a7d3724171
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-06-04 08:47:31 +0800

    Update win32.go

--- winapi/win32.go
+++ winapi/win32.go
@@ -1,179 +1,45 @@
-package winapi
-
-import (
-	"strings"
-	"syscall"
-	"unsafe"
-)
-
 var (
-	user32                       = syscall.NewLazyDLL("user32.dll")
-	procRegisterHotKey           = user32.NewProc("RegisterHotKey")
-	procUnregisterHotKey         = user32.NewProc("UnregisterHotKey")
-	procGetMessage               = user32.NewProc("GetMessageW")
-	
-	advapi32                     = syscall.NewLazyDLL("advapi32.dll")
-	procRegOpenKeyEx             = advapi32.NewProc("RegOpenKeyExW")
-	procRegCloseKey              = advapi32.NewProc("RegCloseKey")
-	procRegQueryValueEx          = advapi32.NewProc("RegQueryValueExW")
-	procRegSetKeyValue           = advapi32.NewProc("RegSetKeyValueW")
-	procRegDeleteValue           = advapi32.NewProc("RegDeleteValueW")
-	procRegNotifyChangeKeyValue  = advapi32.NewProc("RegNotifyChangeKeyValue")
-
-	kernel32                     = syscall.NewLazyDLL("kernel32.dll")
-	procCreateToolhelp32Snapshot = kernel32.NewProc("CreateToolhelp32Snapshot")
-	procProcess32First           = kernel32.NewProc("Process32FirstW")
-	procProcess32Next            = kernel32.NewProc("Process32NextW")
-	procOpenProcess              = kernel32.NewProc("OpenProcess")
-	procTerminateProcess         = kernel32.NewProc("TerminateProcess")
-	procCloseHandle              = kernel32.NewProc("CloseHandle")
-	procCreateEvent              = kernel32.NewProc("CreateEventW")
-	procWaitForSingleObject      = kernel32.NewProc("WaitForSingleObject")
-	procResetEvent               = kernel32.NewProc("ResetEvent")
+	// 在 user32 声明中增加以下核心 API
+	procSetWindowsHookExW   = user32.NewProc("SetWindowsHookExW")
+	procUnhookWindowsHookEx = user32.NewProc("UnhookWindowsHookEx")
+	procCallNextHookEx      = user32.NewProc("CallNextHookEx")
 )
 
 const (
-	HKEY_CURRENT_USER          = 0x80000001
-	KEY_QUERY_VALUE            = 0x0001
-	KEY_NOTIFY                 = 0x0010
-	KEY_SET_VALUE              = 0x0002
-	REG_SZ                     = 1
-	REG_DWORD                  = 4
-	REG_NOTIFY_CHANGE_LAST_SET = 0x00000004
-	WAIT_OBJECT_0              = 0x00000000
-	INFINITE                   = 0xFFFFFFFF
-	TH32CS_SNAPPROCESS         = 0x00000002
-	PROCESS_TERMINATE          = 0x0001
+	WH_KEYBOARD_LL = 13
+	WM_KEYDOWN     = 0x0100
+	VK_SHIFT       = 0x10
+	VK_CONTROL     = 0x11
 )
 
-type TagMSG struct {
-	Hwnd, Message, Wparam, Lparam, Time uint32
-	Pt                                  struct{ X, Y int32 }
+// KBDLLHOOKSTRUCT 键盘钩子结构体
+type KBDLLHOOKSTRUCT struct {
+	VkCode      uint32
+	ScanCode    uint32
+	Flags       uint32
+	Time        uint32
+	DwExtraInfo uintptr
 }
 
-type TagPROCESSENTRY32W struct {
-	Size, Usage, ProcessID   uint32
-	DefaultHeapID            uintptr
-	ModuleID, Threads, Parent uint32
-	PriClassBase, Flags      int32
-	ExeFile                  [260]uint16
+// 暴露出底层的原生调用
+func SetWindowsHookEx(idHook int, lpfn uintptr, hmod uintptr, dwThreadId uint32) syscall.Handle {
+	r1, _, _ := procSetWindowsHookExW.Call(uintptr(idHook), lpfn, hmod, uintptr(dwThreadId))
+	return syscall.Handle(r1)
 }
 
-func RegisterHotKey(id int, modifiers, vk uint32) bool {
-	r1, _, _ := procRegisterHotKey.Call(0, uintptr(id), uintptr(modifiers), uintptr(vk))
+func UnhookWindowsHookEx(hhk syscall.Handle) bool {
+	r1, _, _ := procUnhookWindowsHookEx.Call(uintptr(hhk))
 	return r1 != 0
 }
 
-func UnregisterHotKey(id int) {
-	procUnregisterHotKey.Call(0, uintptr(id))
+func CallNextHookEx(hhk syscall.Handle, nCode int, wParam uintptr, lParam uintptr) uintptr {
+	r1, _, _ := procCallNextHookEx.Call(uintptr(hhk), uintptr(nCode), wParam, lParam)
+	return r1
 }
 
-func GetMessage(msg *TagMSG) int32 {
-	r1, _, _ := procGetMessage.Call(uintptr(unsafe.Pointer(msg)), 0, 0, 0)
-	return int32(r1)
-}
-
-func CreateEvent() syscall.Handle {
-	h, _, _ := procCreateEvent.Call(0, 1, 0, 0)
-	return syscall.Handle(h)
-}
-
-func CloseHandle(h syscall.Handle) {
-	procCloseHandle.Call(uintptr(h))
-}
-
-func WaitForSingleObject(h syscall.Handle, ms uint32) uint32 {
-	r1, _, _ := procWaitForSingleObject.Call(uintptr(h), uintptr(ms))
-	return uint32(r1)
-}
-
-func ResetEvent(h syscall.Handle) {
-	procResetEvent.Call(uintptr(h))
-}
-
-func RegOpenKeyEx(hKey uintptr, path string, samDesired uint32) (syscall.Handle, error) {
-	var out syscall.Handle
-	pathPtr, _ := syscall.UTF16PtrFromString(path)
-	r1, _, _ := procRegOpenKeyEx.Call(hKey, uintptr(unsafe.Pointer(pathPtr)), 0, uintptr(samDesired), uintptr(unsafe.Pointer(&out)))
-	if r1 != 0 {
-		return 0, syscall.Errno(r1)
-	}
-	return out, nil
-}
-
-func RegCloseKey(hKey syscall.Handle) {
-	procRegCloseKey.Call(uintptr(hKey))
-}
-
-func RegQueryValueExDWORD(hKey syscall.Handle, valueName string) (uint32, error) {
-	valuePtr, _ := syscall.UTF16PtrFromString(valueName)
-	var value, size uint32 = 0, 4
-	r1, _, _ := procRegQueryValueEx.Call(uintptr(hKey), uintptr(unsafe.Pointer(valuePtr)), 0, 0, uintptr(unsafe.Pointer(&value)), uintptr(unsafe.Pointer(&size)))
-	if r1 != 0 {
-		return 0, syscall.Errno(r1)
-	}
-	return value, nil
-}
-
-func RegSetKeyValueDWORD(path, valueName string, value uint32) {
-	pathPtr, _ := syscall.UTF16PtrFromString(path)
-	valuePtr, _ := syscall.UTF16PtrFromString(valueName)
-	procRegSetKeyValue.Call(HKEY_CURRENT_USER, uintptr(unsafe.Pointer(pathPtr)), uintptr(unsafe.Pointer(valuePtr)), REG_DWORD, uintptr(unsafe.Pointer(&value)), 4)
-}
-
-func RegNotifyChangeKeyValue(hKey syscall.Handle, hEvent syscall.Handle) {
-	procRegNotifyChangeKeyValue.Call(uintptr(hKey), 0, REG_NOTIFY_CHANGE_LAST_SET, uintptr(hEvent), 1)
-}
-
-func RegDeleteValue(hKey syscall.Handle, valueName string) {
-	valuePtr, _ := syscall.UTF16PtrFromString(valueName)
-	procRegDeleteValue.Call(uintptr(hKey), uintptr(unsafe.Pointer(valuePtr)))
-}
-
-func KillOldInstances(targetExeName string, currentPID uint32) {
-	snapshot, _, _ := procCreateToolhelp32Snapshot.Call(TH32CS_SNAPPROCESS, 0)
-	if syscall.Handle(snapshot) == syscall.InvalidHandle {
-		return
-	}
-	defer procCloseHandle.Call(snapshot)
-
-	var entry TagPROCESSENTRY32W
-	entry.Size = uint32(unsafe.Sizeof(entry))
-
-	ret, _, _ := procProcess32First.Call(snapshot, uintptr(unsafe.Pointer(&entry)))
-	for ret != 0 {
-		exeName := syscall.UTF16ToString(entry.ExeFile[:])
-		if strings.EqualFold(exeName, targetExeName) && entry.ProcessID != currentPID {
-			hProcess, _, _ := procOpenProcess.Call(PROCESS_TERMINATE, 0, uintptr(entry.ProcessID))
-			if hProcess != 0 {
-				procTerminateProcess.Call(hProcess, 0)
-				procCloseHandle.Call(hProcess)
-			}
-		}
-		ret, _, _ = procProcess32Next.Call(snapshot, uintptr(unsafe.Pointer(&entry)))
-	}
-}
-
-func IsAutoStartEnabled(path, key string) bool {
-	hKey, err := RegOpenKeyEx(HKEY_CURRENT_USER, path, KEY_QUERY_VALUE)
-	if err != nil {
-		return false
-	}
-	defer RegCloseKey(hKey)
-
-	valuePtr, _ := syscall.UTF16PtrFromString(key)
-	var size uint32 = 520
-	buf := make([]uint16, 260)
-	r1, _, _ := procRegQueryValueEx.Call(uintptr(hKey), uintptr(unsafe.Pointer(valuePtr)), 0, 0, uintptr(unsafe.Pointer(&buf[0])), uintptr(unsafe.Pointer(&size)))
-	return r1 == 0
-}
-
-func EnableAutoStart(path, key, exePath string) {
-	pathPtr, _ := syscall.UTF16PtrFromString(path)
-	valuePtr, _ := syscall.UTF16PtrFromString(key)
-	exePathStr := `"` + exePath + `"`
-	exePathPtr, _ := syscall.UTF16PtrFromString(exePathStr)
-	size := uint32((len(exePathStr) + 1) * 2)
-
-	procRegSetKeyValue.Call(HKEY_CURRENT_USER, uintptr(unsafe.Pointer(pathPtr)), uintptr(unsafe.Pointer(valuePtr)), REG_SZ, uintptr(unsafe.Pointer(exePathPtr)), uintptr(size))
+// 补充一个获取异步按键状态的 API，用来精准判断 Ctrl 和 Shift 是否同时按下
+var procGetAsyncKeystate = user32.NewProc("GetAsyncKeyState")
+func GetAsyncKeyState(vKey int) int16 {
+	r1, _, _ := procGetAsyncKeystate.Call(uintptr(vKey))
+	return int16(r1)
 }
