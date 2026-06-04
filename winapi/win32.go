adb171523625e067a23574f3d9e3d7598d65df78
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-06-04 09:17:50 +0800

    Update win32.go

--- winapi/win32.go
+++ winapi/win32.go
@@ -1,7 +1,6 @@
 package winapi
 
 import (
-	"strings"
 	"syscall"
 	"unsafe"
 )
@@ -24,34 +23,28 @@ var (
 	procRegCloseKey              = advapi32.NewProc("RegCloseKey")
 	procRegQueryValueEx          = advapi32.NewProc("RegQueryValueExW")
 	procRegSetKeyValue           = advapi32.NewProc("RegSetKeyValueW")
-	procRegDeleteValue           = advapi32.NewProc("RegDeleteValueW")
+	procRegDeleteKeyValue        = advapi32.NewProc("RegDeleteKeyValueW")
 	procRegNotifyChangeKeyValue  = advapi32.NewProc("RegNotifyChangeKeyValue")
 
 	kernel32                     = syscall.NewLazyDLL("kernel32.dll")
-	procCreateToolhelp32Snapshot = kernel32.NewProc("CreateToolhelp32Snapshot")
-	procProcess32First           = kernel32.NewProc("Process32FirstW")
-	procProcess32Next            = kernel32.NewProc("Process32NextW")
-	procOpenProcess              = kernel32.NewProc("OpenProcess")
-	procTerminateProcess         = kernel32.NewProc("TerminateProcess")
-	procCloseHandle              = kernel32.NewProc("CloseHandle")
+	procCreateMutex              = kernel32.NewProc("CreateMutexW")
 	procCreateEvent              = kernel32.NewProc("CreateEventW")
 	procWaitForSingleObject      = kernel32.NewProc("WaitForSingleObject")
 	procResetEvent               = kernel32.NewProc("ResetEvent")
+	procCloseHandle              = kernel32.NewProc("CloseHandle")
 )
 
 const (
 	HKEY_CURRENT_USER          = 0x80000001
 	KEY_QUERY_VALUE            = 0x0001
-	KEY_SET_VALUE              = 0x0002
 	KEY_NOTIFY                 = 0x0010
-	REG_SZ                     = 1
 	REG_DWORD                  = 4
+	REG_SZ                     = 1
 	REG_NOTIFY_CHANGE_LAST_SET = 0x00000004
 	WAIT_OBJECT_0              = 0x00000000
-	INFINITE                   = 0xFFFFFFFF
-	TH32CS_SNAPPROCESS         = 0x00000002
-	PROCESS_TERMINATE          = 0x0001
 	HWND_MESSAGE               = ^uintptr(2)
+	WM_HOTKEY                  = 0x0312
+	WM_DESTROY                 = 0x0002
 )
 
 type TagMSG struct {
@@ -76,17 +69,10 @@ type TagWNDCLASS struct {
 	LpszClassName *uint16
 }
 
-type TagPROCESSENTRY32W struct {
-	Size            uint32
-	Usage           uint32
-	ProcessID       uint32
-	DefaultHeapID   uintptr
-	ModuleID        uint32
-	Threads         uint32
-	ParentProcessID uint32
-	PriClassBase    int32
-	Flags           uint32
-	ExeFile         [260]uint16
+func CreateMutex(name string) (uintptr, error) {
+	namePtr, _ := syscall.UTF16PtrFromString(name)
+	ret, _, _ := procCreateMutex.Call(0, 1, uintptr(unsafe.Pointer(namePtr)))
+	return ret, syscall.GetLastError()
 }
 
 func RegisterClass(className string, wndProc func(hwnd uintptr, msg uint32, wparam uintptr, lparam uintptr) uintptr) {
@@ -160,38 +146,14 @@ func ResetEvent(h syscall.Handle) {
 	procResetEvent.Call(uintptr(h))
 }
 
-func KillOldInstances(targetExeName string, currentPID uint32) {
-	snapshot, _, _ := procCreateToolhelp32Snapshot.Call(TH32CS_SNAPPROCESS, 0)
-	if syscall.Handle(snapshot) == syscall.InvalidHandle {
-		return
-	}
-	defer procCloseHandle.Call(snapshot)
-
-	var entry TagPROCESSENTRY32W
-	entry.Size = uint32(unsafe.Sizeof(entry))
-
-	ret, _, _ := procProcess32First.Call(snapshot, uintptr(unsafe.Pointer(&entry)))
-	for ret != 0 {
-		exeName := syscall.UTF16ToString(entry.ExeFile[:])
-		if strings.EqualFold(exeName, targetExeName) && entry.ProcessID != currentPID {
-			hProcess, _, _ := procOpenProcess.Call(PROCESS_TERMINATE, 0, uintptr(entry.ProcessID))
-			if hProcess != 0 {
-				procTerminateProcess.Call(hProcess, 0)
-				procCloseHandle.Call(hProcess)
-			}
-		}
-		ret, _, _ = procProcess32Next.Call(snapshot, uintptr(unsafe.Pointer(&entry)))
-	}
-}
-
 func RegOpenKeyEx(hKey uintptr, path string, samDesired uint32) (syscall.Handle, error) {
-	var out syscall.Handle
+	var out uintptr
 	pathPtr, _ := syscall.UTF16PtrFromString(path)
 	r1, _, _ := procRegOpenKeyEx.Call(hKey, uintptr(unsafe.Pointer(pathPtr)), 0, uintptr(samDesired), uintptr(unsafe.Pointer(&out)))
 	if r1 != 0 {
 		return 0, syscall.Errno(r1)
 	}
-	return out, nil
+	return syscall.Handle(out), nil
 }
 
 func RegCloseKey(hKey syscall.Handle) {
@@ -208,41 +170,35 @@ func RegQueryValueExDWORD(hKey syscall.Handle, valueName string) (uint32, error)
 	return value, nil
 }
 
-func RegSetKeyValueDWORD(path, valueName string, value uint32) {
-	pathPtr, _ := syscall.UTF16PtrFromString(path)
+func RegQueryValueExSZ(hKey syscall.Handle, valueName string) bool {
 	valuePtr, _ := syscall.UTF16PtrFromString(valueName)
-	procRegSetKeyValue.Call(HKEY_CURRENT_USER, uintptr(unsafe.Pointer(pathPtr)), uintptr(unsafe.Pointer(valuePtr)), REG_DWORD, uintptr(unsafe.Pointer(&value)), 4)
-}
-
-func RegNotifyChangeKeyValue(hKey syscall.Handle, hEvent syscall.Handle) {
-	procRegNotifyChangeKeyValue.Call(uintptr(hKey), 0, REG_NOTIFY_CHANGE_LAST_SET, uintptr(hEvent), 1)
-}
-
-func RegDeleteValue(hKey syscall.Handle, valueName string) {
-	valuePtr, _ := syscall.UTF16PtrFromString(valueName)
-	procRegDeleteValue.Call(uintptr(hKey), uintptr(unsafe.Pointer(valuePtr)))
-}
-
-func IsAutoStartEnabled(path, key string) bool {
-	hKey, err := RegOpenKeyEx(HKEY_CURRENT_USER, path, KEY_QUERY_VALUE)
-	if err != nil {
-		return false
-	}
-	defer RegCloseKey(hKey)
-
-	valuePtr, _ := syscall.UTF16PtrFromString(key)
 	var size uint32 = 520
 	buf := make([]uint16, 260)
 	r1, _, _ := procRegQueryValueEx.Call(uintptr(hKey), uintptr(unsafe.Pointer(valuePtr)), 0, 0, uintptr(unsafe.Pointer(&buf[0])), uintptr(unsafe.Pointer(&size)))
 	return r1 == 0
 }
 
-func EnableAutoStart(path, key, exePath string) {
+func RegSetKeyValueDWORD(path, valueName string, value uint32) {
 	pathPtr, _ := syscall.UTF16PtrFromString(path)
-	valuePtr, _ := syscall.UTF16PtrFromString(key)
-	exePathStr := `"` + exePath + `"`
-	exePathPtr, _ := syscall.UTF16PtrFromString(exePathStr)
-	size := uint32((len(exePathStr) + 1) * 2)
+	valuePtr, _ := syscall.UTF16PtrFromString(valueName)
+	procRegSetKeyValue.Call(HKEY_CURRENT_USER, uintptr(unsafe.Pointer(pathPtr)), uintptr(unsafe.Pointer(valuePtr)), REG_DWORD, uintptr(unsafe.Pointer(&value)), 4)
+}
 
-	procRegSetKeyValue.Call(HKEY_CURRENT_USER, uintptr(unsafe.Pointer(pathPtr)), uintptr(unsafe.Pointer(valuePtr)), REG_SZ, uintptr(unsafe.Pointer(exePathPtr)), uintptr(size))
+func RegSetKeyValueSZ(path, valueName, valueStr string) {
+	pathPtr, _ := syscall.UTF16PtrFromString(path)
+	valuePtr, _ := syscall.UTF16PtrFromString(valueName)
+	valStr := `"` + valueStr + `"`
+	valPtr, _ := syscall.UTF16PtrFromString(valStr)
+	size := uint32((len(valStr) + 1) * 2)
+	procRegSetKeyValue.Call(HKEY_CURRENT_USER, uintptr(unsafe.Pointer(pathPtr)), uintptr(unsafe.Pointer(valuePtr)), REG_SZ, uintptr(unsafe.Pointer(valPtr)), uintptr(size))
+}
+
+func RegDeleteKeyValue(path, valueName string) {
+	pathPtr, _ := syscall.UTF16PtrFromString(path)
+	valuePtr, _ := syscall.UTF16PtrFromString(valueName)
+	procRegDeleteKeyValue.Call(HKEY_CURRENT_USER, uintptr(unsafe.Pointer(pathPtr)), uintptr(unsafe.Pointer(valuePtr)))
+}
+
+func RegNotifyChangeKeyValue(hKey syscall.Handle, hEvent syscall.Handle) {
+	procRegNotifyChangeKeyValue.Call(uintptr(hKey), 0, REG_NOTIFY_CHANGE_LAST_SET, uintptr(hEvent), 1)
 }
