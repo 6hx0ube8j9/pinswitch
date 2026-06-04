50928631f5e03a22e5c6133ee927faa362b9607e
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-06-04 09:02:03 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,14 +3,24 @@
 package main
 
 import (
-	"os"
+	"syscall"
 	"pinswitch/core"
 	"pinswitch/winapi"
 	"pinswitch/ui"
 )
 
+var (
+	kernel32        = syscall.NewLazyDLL("kernel32.dll")
+	procCreateMutex = kernel32.NewProc("CreateMutexW")
+)
+
 func main() {
-	winapi.KillOldInstances("pinswitch.exe", uint32(os.Getpid()))
+	mutexName, _ := syscall.UTF16PtrFromString("Local\\PinswitchUniqueMutexSecure")
+	ret, _, _ := procCreateMutex.Call(0, 1, uintptr(unsafe.Pointer(mutexName)))
+	
+	if ret == 0 || syscall.GetLastError() == syscall.Errno(183) {
+		return
+	}
 
 	engine := core.NewSwitchEngine()
 	tray := ui.NewTrayUI(engine)
