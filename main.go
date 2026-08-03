2d7f22e7d1b2242a44fedcf0021ec9565c1f1bb9
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-08-03 17:48:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -31,11 +31,11 @@ type SwitchBrain struct {
 	lastToggle     time.Time
 	lastToggleHide time.Time
 	toggleMu       sync.Mutex
-	OnReady       func()
-	OnTrayToggle  func(hide bool)
-	OnTrayEvent   func(lparam int)
-	OnMenuCommand func(cmdID int)
-	OnModeChanged func()
+	OnReady        func()
+	OnTrayToggle   func(hide bool)
+	OnTrayEvent    func(lparam int)
+	OnMenuCommand  func(cmdID int)
+	OnModeChanged  func()
 }
 
 func NewSwitchBrain() *SwitchBrain {
@@ -46,17 +46,25 @@ func NewSwitchBrain() *SwitchBrain {
 
 func (b *SwitchBrain) GetIMEMode() uint32 {
 	k, err := registry.OpenKey(registry.CURRENT_USER, RegPathInput, registry.QUERY_VALUE)
-	if err != nil { return 0 }
+	if err != nil {
+		return 0
+	}
 	defer k.Close()
 	val, _, err := k.GetIntegerValue(RegValInput)
-	if err != nil { return 0 }
+	if err != nil {
+		return 0
+	}
 	return uint32(val)
 }
 
 func (b *SwitchBrain) SetIMEMode(mode uint32) bool {
-	if b.GetIMEMode() == mode { return false }
+	if b.GetIMEMode() == mode {
+		return false
+	}
 	k, err := registry.OpenKey(registry.CURRENT_USER, RegPathInput, registry.SET_VALUE)
-	if err != nil { return false }
+	if err != nil {
+		return false
+	}
 	defer k.Close()
 	err = k.SetDWordValue(RegValInput, mode)
 	return err == nil
@@ -82,7 +90,9 @@ func (b *SwitchBrain) ToggleMode() {
 
 func (b *SwitchBrain) IsAutoStart() bool {
 	k, err := registry.OpenKey(registry.CURRENT_USER, RegPathRun, registry.QUERY_VALUE)
-	if err != nil { return false }
+	if err != nil {
+		return false
+	}
 	defer k.Close()
 	_, _, err = k.GetStringValue(RegValRun)
 	return err == nil
@@ -90,7 +100,9 @@ func (b *SwitchBrain) IsAutoStart() bool {
 
 func (b *SwitchBrain) ToggleAutoStart() {
 	k, err := registry.OpenKey(registry.CURRENT_USER, RegPathRun, registry.SET_VALUE)
-	if err != nil { return }
+	if err != nil {
+		return
+	}
 	defer k.Close()
 	if b.IsAutoStart() {
 		k.DeleteValue(RegValRun)
@@ -104,7 +116,9 @@ func (b *SwitchBrain) ToggleAutoStart() {
 
 func (b *SwitchBrain) IsTrayHidden() bool {
 	k, err := registry.OpenKey(registry.CURRENT_USER, RegPathApp, registry.QUERY_VALUE)
-	if err != nil { return false }
+	if err != nil {
+		return false
+	}
 	defer k.Close()
 	val, _, err := k.GetIntegerValue(RegValHideTray)
 	return err == nil && val == 1
@@ -112,9 +126,15 @@ func (b *SwitchBrain) IsTrayHidden() bool {
 
 func (b *SwitchBrain) SetTrayHidden(hide bool) {
 	k, _, err := registry.CreateKey(registry.CURRENT_USER, RegPathApp, registry.ALL_ACCESS)
-	if err != nil { return }
+	if err != nil {
+		return
+	}
 	defer k.Close()
-	if hide { k.SetDWordValue(RegValHideTray, 1) } else { k.SetDWordValue(RegValHideTray, 0) }
+	if hide {
+		k.SetDWordValue(RegValHideTray, 1)
+	} else {
+		k.SetDWordValue(RegValHideTray, 0)
+	}
 }
 
 func (b *SwitchBrain) ToggleHide() {
@@ -143,18 +163,28 @@ func (b *SwitchBrain) StartHotkeyListener() {
 		switch msg {
 		case WM_HOTKEY:
 			switch int(wparam) {
-			case HotkeyToggleMode: b.ToggleMode()
-			case HotkeyToggleHide: b.ToggleHide()
+			case HotkeyToggleMode:
+				b.ToggleMode()
+			case HotkeyToggleHide:
+				b.ToggleHide()
 			}
 			return 0
 		case WM_TRAYICON:
-			if b.OnTrayEvent != nil { b.OnTrayEvent(int(lparam)) }
+			if b.OnTrayEvent != nil {
+				b.OnTrayEvent(int(lparam))
+			}
 			return 0
 		case WM_COMMAND:
-			if b.OnMenuCommand != nil { b.OnMenuCommand(int(wparam & 0xFFFF)) }
+			if b.OnMenuCommand != nil {
+				b.OnMenuCommand(int(wparam & 0xFFFF))
+			}
 			return 0
-		case WM_USER + 777: b.ToggleMode(); return 0
-		case WM_USER + 778: b.ToggleHide(); return 0
+		case WM_USER + 777:
+			b.ToggleMode()
+			return 0
+		case WM_USER + 778:
+			b.ToggleHide()
+			return 0
 		case WM_CLOSE:
 			UnregisterHotKey(hwnd, HotkeyToggleMode)
 			UnregisterHotKey(hwnd, HotkeyToggleHide)
@@ -166,10 +196,14 @@ func (b *SwitchBrain) StartHotkeyListener() {
 	})
 
 	hwnd := CreateWindowEx(0, className, "PinswitchHotkey", 0, 0, 0, 0, 0, HWND_MESSAGE, 0, 0, 0)
-	if hwnd == 0 { return }
+	if hwnd == 0 {
+		return
+	}
 	b.hwnd = hwnd
 
-	if b.OnReady != nil { b.OnReady() }
+	if b.OnReady != nil {
+		b.OnReady()
+	}
 
 	RegisterHotKey(hwnd, HotkeyToggleMode, 0x0002|0x0004, 0x59)
 	RegisterHotKey(hwnd, HotkeyToggleHide, 0x0004|0x0002|0x0008, 0x59)
@@ -177,7 +211,9 @@ func (b *SwitchBrain) StartHotkeyListener() {
 	var msg Msg
 	for {
 		ret := GetMessage(&msg, 0, 0, 0)
-		if ret == 0 || ret == -1 { break }
+		if ret == 0 || ret == -1 {
+			break
+		}
 		TranslateMessage(&msg)
 		DispatchMessage(&msg)
 	}
@@ -185,11 +221,20 @@ func (b *SwitchBrain) StartHotkeyListener() {
 
 func (b *SwitchBrain) WatchRegistry(ctx context.Context, onChanged func()) {
 	k, err := registry.OpenKey(registry.CURRENT_USER, RegPathInput, registry.NOTIFY)
-	if err != nil { return }
+	if err != nil {
+		return
+	}
 	regEvent, err := windows.CreateEvent(nil, 0, 0, nil)
-	if err != nil { k.Close(); return }
+	if err != nil {
+		k.Close()
+		return
+	}
 	quitEvent, err := windows.CreateEvent(nil, 0, 0, nil)
-	if err != nil { windows.CloseHandle(regEvent); k.Close(); return }
+	if err != nil {
+		windows.CloseHandle(regEvent)
+		k.Close()
+		return
+	}
 
 	watchCtx, cancel := context.WithCancel(ctx)
 	exited := make(chan struct{})
@@ -211,10 +256,16 @@ func (b *SwitchBrain) WatchRegistry(ctx context.Context, onChanged func()) {
 	events := []windows.Handle{regEvent, quitEvent}
 	for {
 		err = windows.RegNotifyChangeKeyValue(windows.Handle(k), false, windows.REG_NOTIFY_CHANGE_LAST_SET, regEvent, true)
-		if err != nil { return }
+		if err != nil {
+			return
+		}
 		s, err := windows.WaitForMultipleObjects(events, false, windows.INFINITE)
-		if err != nil || s == windows.WAIT_OBJECT_0+1 { return }
-		if s == windows.WAIT_OBJECT_0 { onChanged() }
+		if err != nil || s == windows.WAIT_OBJECT_0+1 {
+			return
+		}
+		if s == windows.WAIT_OBJECT_0 {
+			onChanged()
+		}
 	}
 }
 
@@ -231,7 +282,11 @@ func main() {
 		}
 		return
 	}
-	defer func() { if ret != 0 { CloseHandle(syscall.Handle(ret)) } }()
+	defer func() {
+		if ret != 0 {
+			CloseHandle(syscall.Handle(ret))
+		}
+	}()
 
 	brain := NewSwitchBrain()
 	tray := NewTrayUI(brain)
@@ -252,7 +307,11 @@ func main() {
 		tray.HandleMenuClick(cmdID)
 	}
 	brain.OnTrayToggle = func(hide bool) {
-		if hide { tray.Hide() } else { tray.Show() }
+		if hide {
+			tray.Hide()
+		} else {
+			tray.Show()
+		}
 	}
 	brain.OnModeChanged = func() {
 		tray.SyncUI(NIM_MODIFY)
@@ -271,5 +330,5 @@ func main() {
 	})
 
 	<-exitChan
-	tray.Hide()
+	tray.Close()
 }
