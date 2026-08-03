8a9b2aea7b24e382245e5f769828fdc34163d71b
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-08-03 12:23:47 +0800

    Update tray.go

--- tray.go
+++ tray.go
@@ -3,13 +3,11 @@
 package main
 
 import (
-	"context"
 	_ "embed"
 	"os"
-	"os/signal"
+	"path/filepath"
 	"syscall"
-
-	"github.com/energye/systray"
+	"unsafe"
 )
 
 //go:embed icons/quan.ico
@@ -18,118 +16,132 @@ var iconQuan []byte
 //go:embed icons/shuang.ico
 var iconShuang []byte
 
+const (
+	MenuIDFullPinyin   = 1001
+	MenuIDDoublePinyin = 1002
+	MenuIDAutoStart    = 1003
+	MenuIDHelp         = 1004
+	MenuIDExit         = 1005
+)
+
 type TrayUI struct {
-	brain         *SwitchBrain
-	mFullPinyin   *systray.MenuItem
-	mDoublePinyin *systray.MenuItem
-	mAutoStart    *systray.MenuItem
-	ctx           context.Context
-	cancel        context.CancelFunc
-	currentMode   uint32
+	brain       *SwitchBrain
+	hIconQuan   uintptr
+	hIconShuang uintptr
+	isVisible   bool
 }
 
 func NewTrayUI(brain *SwitchBrain) *TrayUI {
-	ctx, cancel := context.WithCancel(context.Background())
 	return &TrayUI{
 		brain:       brain,
-		ctx:         ctx,
-		cancel:      cancel,
-		currentMode: 999,
+		hIconQuan:   loadTempIcon(iconQuan, "pinswitch_quan.ico"),
+		hIconShuang: loadTempIcon(iconShuang, "pinswitch_shuang.ico"),
+		isVisible:   false,
 	}
 }
 
-func (t *TrayUI) Start() {
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		select {
-		case <-sigs:
-			systray.Quit()
-		case <-t.ctx.Done():
-		}
-	}()
-
-	systray.Run(t.onReady, t.onExit)
+func loadTempIcon(b []byte, name string) uintptr {
+	path := filepath.Join(os.TempDir(), name)
+	_ = os.WriteFile(path, b, 0644)
+	return LoadIconFromPath(path)
 }
 
-func (t *TrayUI) onReady() {
-	systray.SetTitle("pinswitch")
+func (t *TrayUI) Show() {
+	if t.isVisible {
+		return
+	}
+	t.isVisible = true
+	t.SyncUI(NIM_ADD)
+}
 
-	t.mFullPinyin = systray.AddMenuItem("全拼输入", "")
-	t.mDoublePinyin = systray.AddMenuItem("双拼输入", "")
-	systray.AddSeparator()
-	t.mAutoStart = systray.AddMenuItem("开机启动", "")
-	systray.AddSeparator()
-	mHelp := systray.AddMenuItem("快捷键说明", "")
-	mQuit := systray.AddMenuItem("退出程序", "")
+func (t *TrayUI) Hide() {
+	if !t.isVisible {
+		return
+	}
+	t.isVisible = false
+	nid := t.getNotifyData()
+	ShellNotifyIcon(NIM_DELETE, &nid)
+}
 
-	t.SyncUI()
+func (t *TrayUI) SyncUI(action uint32) {
+	if !t.isVisible {
+		return
+	}
+	nid := t.getNotifyData()
+	ShellNotifyIcon(action, &nid)
+}
 
-	go t.brain.WatchRegistry(t.ctx, func() {
-		t.SyncUI()
-	})
+func (t *TrayUI) getNotifyData() NotifyIconData {
+	nid := NotifyIconData{
+		CbSize:           uint32(unsafe.Sizeof(NotifyIconData{})),
+		HWnd:             t.brain.hwnd,
+		UID:              1,
+		UFlags:           NIF_MESSAGE | NIF_ICON | NIF_TIP,
+		UCallbackMessage: WM_TRAYICON,
+	}
 
-	t.mFullPinyin.Click(func() {
+	mode := t.brain.GetIMEMode()
+	tip := "Pinswitch: 全拼模式"
+	nid.HIcon = t.hIconQuan
+
+	if mode == 1 {
+		tip = "Pinswitch: 双拼模式"
+		nid.HIcon = t.hIconShuang
+	}
+
+	copy(nid.SzTip[:], syscall.StringToUTF16(tip))
+	return nid
+}
+
+func (t *TrayUI) ShowMenu() {
+	SetForegroundWindow(t.brain.hwnd)
+	hMenu := CreatePopupMenu()
+
+	mode := t.brain.GetIMEMode()
+	quanFlag := uint32(MF_STRING)
+	shuangFlag := uint32(MF_STRING)
+	if mode == 0 {
+		quanFlag |= MF_CHECKED | MF_GRAYED
+	} else {
+		shuangFlag |= MF_CHECKED | MF_GRAYED
+	}
+
+	autoFlag := uint32(MF_STRING)
+	if t.brain.IsAutoStart() {
+		autoFlag |= MF_CHECKED
+	}
+
+	AppendMenu(hMenu, quanFlag, MenuIDFullPinyin, "全拼输入")
+	AppendMenu(hMenu, shuangFlag, MenuIDDoublePinyin, "双拼输入")
+	AppendMenu(hMenu, MF_SEPARATOR, 0, "")
+	AppendMenu(hMenu, autoFlag, MenuIDAutoStart, "开机启动")
+	AppendMenu(hMenu, MF_SEPARATOR, 0, "")
+	AppendMenu(hMenu, MF_STRING, MenuIDHelp, "快捷键说明")
+	AppendMenu(hMenu, MF_STRING, MenuIDExit, "退出程序")
+
+	pt := GetCursorPos()
+	TrackPopupMenu(hMenu, TPM_BOTTOMALIGN|TPM_LEFTALIGN|TPM_RIGHTBUTTON, pt.X, pt.Y, 0, t.brain.hwnd, 0)
+	PostMessage(t.brain.hwnd, WM_USER, 0, 0)
+	DestroyMenu(hMenu)
+}
+
+func (t *TrayUI) HandleMenuClick(cmdID int) {
+	switch cmdID {
+	case MenuIDFullPinyin:
 		t.brain.SetIMEMode(0)
-	})
-
-	t.mDoublePinyin.Click(func() {
+		t.SyncUI(NIM_MODIFY)
+	case MenuIDDoublePinyin:
 		t.brain.SetIMEMode(1)
-	})
-
-	t.mAutoStart.Click(func() {
+		t.SyncUI(NIM_MODIFY)
+	case MenuIDAutoStart:
 		t.brain.ToggleAutoStart()
-		t.SyncUI()
-	})
-
-	mHelp.Click(func() {
+	case MenuIDHelp:
 		helpText := "【快捷键说明】\n\n" +
 			"Shift+Ctrl+Y：切换全拼/双拼\n" +
 			"Shift+Ctrl+Win+Y：显示/隐藏托盘图标\n" +
 			"Shift+双击程序：显示/隐藏托盘图标"
 		MessageBox(0, helpText, "Pinswitch", 0x00000040)
-	})
-
-	mQuit.Click(func() {
-		systray.Quit()
-	})
-
-	systray.SetOnClick(func(menu systray.IMenu) {
-		t.brain.ToggleMode()
-	})
-}
-
-func (t *TrayUI) onExit() {
-	t.cancel()
-	t.brain.Close()
-}
-
-func (t *TrayUI) SyncUI() {
-	mode := t.brain.GetIMEMode()
-
-	if mode != t.currentMode {
-		t.currentMode = mode
-		if mode == 1 {
-			systray.SetIcon(iconShuang)
-			systray.SetTooltip("当前: 双拼模式")
-			t.mDoublePinyin.Check()
-			t.mDoublePinyin.Disable()
-			t.mFullPinyin.Uncheck()
-			t.mFullPinyin.Enable()
-		} else {
-			systray.SetIcon(iconQuan)
-			systray.SetTooltip("当前: 全拼模式")
-			t.mFullPinyin.Check()
-			t.mFullPinyin.Disable()
-			t.mDoublePinyin.Uncheck()
-			t.mDoublePinyin.Enable()
-		}
-	}
-
-	if t.brain.IsAutoStart() {
-		t.mAutoStart.Check()
-	} else {
-		t.mAutoStart.Uncheck()
+	case MenuIDExit:
+		PostMessage(t.brain.hwnd, WM_CLOSE, 0, 0)
 	}
 }
